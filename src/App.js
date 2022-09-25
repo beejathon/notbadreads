ef82f9360cb8d823dc28c77314fae8eeb20b2b95
Bee-jay <bjpaiz@gmail.com>
2022-09-25 18:31:21 +0100

    set up firebase and tailwind

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { initializeApp } from "firebase/app";
+import { getAuth } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAIVABDn3ZZJIiSt3HhDgtZPa3hcueYqKw",
+  authDomain: "notbadreads.firebaseapp.com",
+  projectId: "notbadreads",
+  storageBucket: "notbadreads.appspot.com",
+  messagingSenderId: "715831450876",
+  appId: "1:715831450876:web:30792b6dd051be1b59cb79"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
 
-export default App;
+export { App, auth };
